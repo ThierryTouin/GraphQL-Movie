3f7d48d5c578ef8334a14a15e14cb0ae5c9507e4
Thierry Touin <thierry.touin@gfi.fr>
2020-04-03 11:49:40 +0200

    requete sur les datas

--- client/components/movie-list.js
+++ client/components/movie-list.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+
 
 class MovieList extends Component {
     render() {
+        console.log(this.props);
         return (
             <div>
-                Liste de film
+                Liste de films
+
+                <ul>
+                    {this.renderMovies()}
+                </ul>
+
             </div>
         )
     }
+
+renderMovies() {
+    if (!this.props.data.loading) {
+        return this.props.data.movies.map( (movie => {
+            return <li key={movie.id}>{movie.title}</li>
+        }))    
+    } else {
+        return "Chargement des données...";
+    }
+    
+    
 }
 
-export default MovieList;
+}
+
+
+const query = gql`{
+    movies{
+        id,
+        title
+    }
+}
+`
+
+export default graphql(query) (MovieList);
